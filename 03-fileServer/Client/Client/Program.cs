f9e0e44cbe1fbf042e6e8c4effdc257fd78dd946
private <user@example.com>
2015-06-29 14:45:44 +0200

    Adds the file server snippet and the queue folder.

---
+++ 03-fileServer/Client/Client/Program.cs
@@ -0,0 +1,106 @@
+﻿/***************************************
+ * Simple CLI Client-Server Manager
+ ***************************************/
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client {
+
+	class Program {
+		
+		static void Main(string[] args) {
+	
+			// Checking input
+			if (args.Length < 3) {
+				Console.WriteLine("\nAvast, ye landlubber! To enter me ship, type: client.exe -i <ID>");
+				Console.ReadLine();
+				return;
+			}
+			int i;
+			if ( !int.TryParse(args[2], out i) ) {
+				Console.WriteLine("\nMe can't read \"" + args[2] + "\", is not a numbarrr!");
+				Console.ReadLine();
+				return;
+			}
+			if (i < 0) {
+				Console.WriteLine("\nMe can't read negative " + i); 
+				Console.ReadLine();
+				return;
+			}
+
+			int counter = 0;
+			string directory = "..\\..\\..\\..\\queue";
+			string command, filename, answer;	
+			
+			do {
+				command = readCommand();
+
+				// If the ID is not 1 and the command is L, discard message.
+				if ( i != 1 && String.Compare(command, "L") == 0 )
+					Console.WriteLine("Swim with the fishes, ye scurvy dog, mwahuahuahaha...");
+
+				// Else, create message.
+				else {
+					try{
+						// Build filename. If file exists, regenerate (not very elegant, BUT)
+						do {
+							// The faster method for string concatenation is Join()
+							filename = String.Join("_", new String[] { args[2], command.ToUpper(), counter.ToString("D5") });
+							filename = String.Join("\\", new String[] { directory, filename });
+							filename = String.Join(".", new String[] { filename, "txt" });
+							counter++;
+							if(counter > 99999)
+								counter = 0;
+						} while ( File.Exists(filename) );
+
+						// Now that filename doesn't exist, create file
+						// Preferred method or best practice for file creation in C#:
+						using ( var newFile = File.Create(filename) ) {};
+						
+						Console.WriteLine("Ship ahoy! It's called '" + filename.Substring(filename.LastIndexOf("\\")+1) + "'."); 
+					
+					} catch (Exception e) {
+						Console.WriteLine("Thar be nothin' in the sea! Unable to create ship");
+						Console.WriteLine(e.ToString());
+					}
+				}
+				
+				Console.Write("\nWrite 'aye' to drink moar rum or 'nope' to leave me ship: ");
+				answer = Console.ReadLine();
+
+			} while(String.Compare(answer,"aye") == 0);
+		}
+	
+		/***************************************
+		* CLIENT READS A COMMAND
+		* Checks if it is "S", "H" or "L"
+		* (sounds like a joke, but it isn't)
+		***************************************/
+		public static string readCommand() {
+			string command = "";
+	
+			Console.WriteLine("\nAhoy matey! choose yer rum:"); 
+			Console.Write("S, H or L?: "); 
+			do {
+				command = Console.ReadLine();
+
+				// Check the input
+				if( String.Compare(command, "S", true) != 0 &&
+					String.Compare(command, "H", true) != 0 &&
+					String.Compare(command, "L", true) != 0) {
+						command = "0";
+						Console.Write("Avast, ye landlubber! Please choose S, H or L?: ");
+				}
+			} while(String.Compare(command, "0") == 0);
+
+			// If the input is correct
+			command = command.ToUpper();
+			Console.WriteLine("Arrr! Here's what ye chose: " + command + "\n"); 
+			return command;
+		}
+	}
+}
---
+++ 03-fileServer/Server/Server/Program.cs
@@ -0,0 +1,170 @@
+﻿/***************************************
+ * Simple CLI Client-Server Manager
+ ***************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// File System Watcher
+using System.IO;
+using System.Security.Permissions;
+
+namespace Server {
+
+	class Program {
+
+		static void Main(string[] args) {
+			Program p = new Program();
+			p.welcome();
+			p.Run();
+		}
+
+		/***************************************
+		* SERVER MAIN ROUTINE
+		***************************************/
+		[PermissionSet(SecurityAction.Demand, Name="FullTrust")]
+		public void Run() {
+
+			// Create a new FileSystemWatcher and set the directory.
+			FileSystemWatcher watcher = new FileSystemWatcher();
+			watcher.Path = "..\\..\\..\\..\\queue";
+
+			// Only check for creation and deletion
+			watcher.NotifyFilter = NotifyFilters.FileName;
+			
+			// Watch any file.
+			watcher.Filter = "*.*";
+
+			// Add event handlers.
+			watcher.Created += new FileSystemEventHandler(OnChanged);
+			watcher.Deleted += new FileSystemEventHandler(OnChanged);
+
+			// Begin watching.
+			watcher.EnableRaisingEvents = true;
+
+			// Wait for the user to quit the program.
+			Console.WriteLine("Press \'q\' to quit.");
+			while (Console.Read()!='q');
+		}
+
+		/***************************************
+		* EVENT HANDLER
+		***************************************/
+		private void OnChanged(object source, FileSystemEventArgs e) {
+			string fullpath = e.FullPath.Substring(0, e.FullPath.LastIndexOf("\\") );
+			Console.Write("\nAvast, "        + fullpath     + "!... \n");
+			Console.Write("Me eaye see: \'"  + e.ChangeType + "\'. ");
+			Console.Write("Ship o' name: \'" + e.Name       + "\'\n\n");
+
+			// Here's the parrrty
+			if (String.Compare(e.ChangeType.ToString(), "Created") == 0) {
+				draw(e.Name);
+				keelhaul(e.FullPath);
+			}
+		}
+
+		/***************************************
+		* SERVER CONTROL CENTER
+		***************************************/
+		private void draw(string filename) {
+			string id = "", command = "", counter = "";
+			decode(filename, out id, out command, out counter);
+
+			if (String.Compare(command, "S", true) == 0) {
+				Console.WriteLine("                  .  ;  ; .                ");
+				Console.WriteLine("                   '  .. '                 ");
+				Console.WriteLine("     _|_          =- {  } -=       _|_     ");
+				Console.WriteLine("    ``|`           .  .. .         `|``    ");
+				Console.WriteLine("   ```|``         '  ;  ; '       ``|```   ");
+				Console.WriteLine("   `__!__    )'             '(    __!__`   ");
+				Console.WriteLine("   :     := },;             ;,{ =:     :   ");
+				Console.WriteLine("   '.   .'                       '.   .'   ");
+				Console.WriteLine("+~-=~~-=~~-=~~-=~~-=~~-=~~-=~~-=~~-=~~-=~~+");
+				Console.WriteLine("|                                         |");
+				Console.WriteLine("|          Ship numbarrr: " + counter + "           |");
+				Console.WriteLine("|                                         |");
+				Console.WriteLine("+~-=~~-=~~-=~~-=~~-=~~-=~~-=~~-=~~-=~~-=~~+\n");
+			} else if (String.Compare(command, "H", true) == 0) {
+				Console.WriteLine("                  .  ;  ; .                ");
+				Console.WriteLine("                   '  .. '                 ");
+				Console.WriteLine("     _|_          =- {  } -=       _|_     ");
+				Console.WriteLine("    ``|`           .  .. .         `|``    ");
+				Console.WriteLine("   ```|``         '  ;  ; '       ``|```   ");
+				Console.WriteLine("   `__!__    )'             '(    __!__`   ");
+				Console.WriteLine("   :     := },;             ;,{ =:     :   ");
+				Console.WriteLine("   '.   .'                       '.   .'   ");
+				Console.WriteLine("+~-=~~-=~~-=~~-=~~-=~~-=~~-=~~-=~~-=~~-=~~+");
+				Console.WriteLine("                -----------                ");
+				Console.WriteLine("               /           \\              ");
+				Console.WriteLine("              /    Ship     \\             ");
+				Console.WriteLine("             /   numbarrr:   \\            ");
+				Console.WriteLine("             \\               /            ");
+				Console.WriteLine("              \\    " + counter + "    /      ");
+				Console.WriteLine("               \\           /              ");
+				Console.WriteLine("                -----------                \n");
+			} else if (String.Compare(command, "L", true) == 0) {
+				if (String.Compare(id, "1") == 0)
+					Console.WriteLine("+~-=~~-=~~-=~~-=~~-=~~-=~~-=~~-=~~-=~~-=~~+\n");
+				else
+					Console.WriteLine("Yo Ho, Yo Ho! A pirates life for me!");
+			}
+		}
+
+		/***************************************
+		* DECODE FILENAME
+		***************************************/
+		private void decode(string filename, out string id, out string command, out string counter) {
+			int found = filename.IndexOf("_");
+			id        = filename.Substring(0, found);
+			command   = filename.Substring(found + 1, 1);
+			counter   = filename.Substring(found + 3, 5);
+		}
+
+		/***************************************
+		* KILL FILES
+		***************************************/
+		private void keelhaul(string path) {
+			try {
+				Console.WriteLine("Keelhauling " + path);
+				File.Delete(path);
+			} catch (Exception e) {
+				Console.WriteLine("Could not keelhaul " + path);
+				Console.WriteLine(e.Message);
+			}
+		}
+
+		/***************************************
+		 * YE OLDEY ASCII ART!
+		***************************************/
+		public void welcome() {
+			Console.WriteLine("\n\n         YE OLDE PIRATE' SERVER!\n");
+            Console.WriteLine("                 uuuuuuu");
+            Console.WriteLine("             uu$$$$$$$$$$$uu");
+            Console.WriteLine("          uu$$$$$$$$$$$$$$$$$uu");
+            Console.WriteLine("         u$$$$$$$$$$$$$$$$$$$$$u");
+            Console.WriteLine("        u$$$$$$$$$$$$$$$$$$$$$$$u");
+            Console.WriteLine("       u$$$$$$$$$$$$$$$$$$$$$$$$$u");
+            Console.WriteLine("       u$$$$$$$$$$$$$$$$$$$$$$$$$u");
+            Console.WriteLine("       u$$$$$$\"   \"$$$\"   \"$$$$$$u");
+            Console.WriteLine("       \"$$$$\"      u$u       $$$$\"");
+            Console.WriteLine("        $$$u       u$u       u$$$");
+            Console.WriteLine("        $$$u      u$$$u      u$$$");
+            Console.WriteLine("         \"$$$$uu$$$   $$$uu$$$$\"");
+            Console.WriteLine("          \"$$$$$$$\"   \"$$$$$$$\"");
+            Console.WriteLine("            u$$$$$$$u$$$$$$$u");
+            Console.WriteLine("             u$\"$\"$\"$\"$\"$\"$u");
+            Console.WriteLine("  uuu        $$u$ $ $ $ $u$$       uuu");
+            Console.WriteLine(" u$$$$        $$$$$u$u$u$$$       u$$$$");
+            Console.WriteLine("  $$$$$uu      \"$$$$$$$$$\"     uu$$$$$$");      
+            Console.WriteLine("u$$$$$$$$$$$uu    \"\"\"\"\"    uuuu$$$$$$$$$$");
+            Console.WriteLine("$$$$\"\"\"$$$$$$$$$$uuu   uu$$$$$$$$$\"\"\"$$$\"");
+            Console.WriteLine(" \"\"\"      \"\"$$$$$$$$$$$uu \"\"$\"\"\"");
+            Console.WriteLine("           uuuu \"\"$$$$$$$$$$uuu");
+            Console.WriteLine("  u$$$uuu$$$$$$$$$uu \"\"$$$$$$$$$$$uuu$$$");
+            Console.WriteLine("  $$$$$$$$$$\"\"\"\"           \"\"$$$$$$$$$$$\"");
+            Console.WriteLine("   \"$$$$$\"                      \"\"$$$$\"\"");
+            Console.WriteLine("     $$$\"                         $$$$\"\n");
+		}
+	}
+}
